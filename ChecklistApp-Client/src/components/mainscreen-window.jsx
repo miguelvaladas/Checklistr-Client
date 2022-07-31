a6959e622e6d15e34dec0f719990e3fbc20e9d26
airesvaladas <airesvaladas@gmail.com>
2022-07-31 01:42:25 +0100

    added main screen components + bootstrap

---
+++ ChecklistApp-Client/src/components/mainscreen-window.jsx
@@ -0,0 +1,21 @@
+// import "../ChecklistApp-Client/src/App.css";
+
+export const MainscreenWindow = () => {
+
+	return (
+	<div>
+		<div id="mainWindow"> 
+		 <button className="mainButton"> 
+			ENTER 
+		</button> 
+
+
+		</div>		
+		
+		</div>
+
+
+	)
+
+
+}
---
+++ ChecklistApp-Client/src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import "./navbar.css";
+
+export const Navbar = () => {
+
+	return(
+	<div id="navbar">
+		<div className="logo"><img src="../../src/assets/placeholder_logo.jpg"></img></div>
+		<div><h1>Welcome to the BestCheckListApp EVER!</h1></div>
+
+
+		</div>
+
+	)
+
+}
