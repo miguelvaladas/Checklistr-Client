66f124bc98c1c483e41f9f962e090a7bb7a112aa
airesvaladas <airesvaladas@gmail.com>
2022-08-21 00:35:35 +0100

    built login page

--- ChecklistApp-Client/src/components/mainmodalbox.jsx
+++ ChecklistApp-Client/src/components/mainmodalbox.jsx
@@ -1,8 +1,37 @@
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
-
+import "../../src/components/mainmodalbox.css";
+import React, { useState } from 'react';
 
 export const MainScreenModal = (props) => {
+
+	// const [showRegister,setShowRegister] = useState(false);		
+	const [showLogin, setShowLogin] = useState(true);
+
+	const Login = () => {
+		return(
+		<div>
+		<h2>Login</h2>	
+		<form>
+			<div><label for="username">Username:</label></div>
+			<div class="formFields"><input type="text" name='username' placeholder=" ✉️ Insert username...">
+			</input></div>
+			<div><label for="password">Password:</label></div>
+
+			<div class="formFields"><input type="text" name="password" placeholder=' 🔑 Insert password...'></input>
+			</div>
+			 <div>
+		<input class="submitButton" type="submit" value="Submit"></input>
+			</div>
+		</form>
+			<p>You do not have an account? Click <a onClick={() => {
+				setShowLogin(false);
+				// setShowRegister(true);
+				}}>here</a> to sign-up.</p>
+		</div>
+		)
+	}
+
 
 	return ( 
 		<Modal {...props}
@@ -12,24 +41,15 @@ export const MainScreenModal = (props) => {
 		>
 		
 		<Modal.Header closeButton>
-			<Modal.Title id="contained-modal-title-vcenter"> HEADING </Modal.Title>
 		</Modal.Header>
 
 		<Modal.Body>
-		<h1>LOTS OF TEXT</h1>
-		<p>BLABLABLABLABALkasjdhfkjashdkjashdkjashdkjashdkjahskdjahskdjhsakjdhaskjdhkasjdhkasjhdkasj</p>
+			{showLogin ? <Login /> : null}	
 		</Modal.Body>
-		
 		<Modal.Footer>
 			<Button onClick={props.onHide}>Close</Button>
 		</Modal.Footer>
-
-
-
-
 		</Modal>
-
-
 	)	
 
 
